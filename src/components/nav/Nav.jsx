bc6ddc7fa2891a4ed755ab970ce6250f952e3045
ivonamojsovska <ivonamojsovska15@gmail.com>
2023-04-04 22:12:53 -0700

    nav added and done

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,51 @@
-import React from "react";
+import { React, useState } from "react";
 import "./Nav.css";
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { BiBook, BiWrench, BiMessageSquareDetail } from "react-icons/bi";
+import { MdWorkOutline } from "react-icons/md";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [activeNav, setActiveNav] = useState("#");
+
+  return (
+    <nav>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <MdWorkOutline />
+      </a>
+      <a
+        href="#projects"
+        onClick={() => setActiveNav("#projects")}
+        className={activeNav === "#projects" ? "active" : ""}
+      >
+        <BiWrench />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <BiMessageSquareDetail />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Portfolio = () => {
-  return <>Portfolio</>;
+  return <section id="portfolio">Portfolio</section>;
 };
 
 export default Portfolio;
