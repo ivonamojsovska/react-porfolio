a2a67354aa8829aac4f51bb56b767141862936d8
ivonamojsovska <ivonamojsovska15@gmail.com>
2023-04-06 12:33:03 -0700

    footer added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,44 @@
+import "./Footer.css";
+import { FaFacebookF, FaTwitter } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+
 const Footer = () => {
-  return <>Footer</>;
+  return (
+    <footer>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#Portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="htpps://facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="htpps://instagram.com">
+          <FiInstagram />
+        </a>
+        <a href="htpps://twitter.com">
+          <FaTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Design by EGATOR.All Rights Reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
