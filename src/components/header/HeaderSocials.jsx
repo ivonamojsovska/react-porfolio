c284e771457b1ebf1d80d105fe76f261a9943ec2
ivonamojsovska <ivonamojsovska15@gmail.com>
2023-04-09 19:15:44 -0700

    socials added

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,13 +4,13 @@ import { BsLinkedin, BsGithub, BsInstagram } from "react-icons/bs";
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://linkedin.com" target="_blank">
+      <a href="https://www.linkedin.com/in/ivonamojsovska/" target="_blank">
         <BsLinkedin />
       </a>
-      <a href="https://github.com" target="_blank">
+      <a href="https://github.com/ivonamojsovska" target="_blank">
         <BsGithub />
       </a>
-      <a href="https://instagram.com" target="_blank">
+      <a href="https://www.instagram.com/shevonnekaraiv/" target="_blank">
         <BsInstagram />
       </a>
     </div>
