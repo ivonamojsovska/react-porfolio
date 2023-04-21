7359600b3097dc96d3a08b9e50751c63d6543d02
ivonamojsovska <ivonamojsovska15@gmail.com>
2023-04-21 14:19:03 -0700

    final touches

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./About.css";
-import ME from "../../assets/DSC07124 (1).jpg";
+import ME from "../../assets/aboutme.jpg";
 import { FaAward } from "react-icons/fa";
 import { TbCertificate } from "react-icons/tb";
 import { VscFolderLibrary } from "react-icons/vsc";
