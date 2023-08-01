b4caf5a9c1e9d3e7ff3e5715caf4cf6ed5dcba5b
ivonamojsovska <ivonamojsovska15@gmail.com>
2023-08-01 12:45:23 -0700

    Description on CV

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from "../../assets/CV.pdf";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
+      <a href={CV} download="Ivona Mojsovska CV" className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
