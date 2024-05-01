4059e2ee2cac4ae8da876c252e344179aff7940a
ivonamojsovska <ivonamojsovska15@gmail.com>
2024-05-01 10:11:30 -0700

    CV updated

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import CV from "../../assets/CV.pdf";
+// import CV from "../../assets/CV.pdf";
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download="Ivona Mojsovska CV" className="btn">
+      <a
+        href="../../assets/IvonaMojsovskaCV.pdf"
+        download="Ivona Mojsovska CV"
+        className="btn"
+      >
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
